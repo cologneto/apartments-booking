99b38438b71492ad4a4c34f0ce31301914c697af
Georgi Tsanev <cologneto@gmail.com>
2019-07-25 08:30:29 +0300

    Added Mock data for apartments and new redirect in routes

---
+++ src/data/apartments.js
@@ -0,0 +1,14 @@
+export const apartments = [
+    {
+        id: 1,
+        name: 'Picture 1',
+        fileURL: './images/ap1/main1.jpg',
+        allPicsURLs: [
+            './images/ap1/pic1.jpg',
+            './images/ap1/pic2.jpg',
+            './images/ap1/pic3.jpg',
+            './images/ap1/pic4.jpg',
+            './images/ap1/pic4.jpg',
+        ]
+    }
+];
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ import Apartments from './components/admin/apartments/Apartments'
 import Bookings from './components/admin/bookings/Bookings'
 
 export const routes = [
-    { path: '/', component: Home },
+    { path: '/home', component: Home },
     { path: '/apartments', component: Apartments },
-    { path: '/bookings', component: Bookings }
+    { path: '/bookings', component: Bookings },
+    { path: '*', redirect: '/home' }
 ]
